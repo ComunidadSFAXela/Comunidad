58e899eb4eca9d3613d21e6f83b881bc24b087b6
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-10-24 13:16:19 -0600

    test2

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -110,7 +110,6 @@ const Login = () => {
                 </RouterLink>
               </div>
               <Button
-                onSubmit={obtenerTipoUsuario}
                 type="submit"
                 onClick={obtenerTipoUsuario}
                 className="w-full text-white bg-danger-600 hover:bg-danger-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
