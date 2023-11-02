4be16a5feeefebe8a7c002049b0f873e338c5ed3
CristopherPaiz <136940619+CristopherPaiz@users.noreply.github.com>
2023-11-02 06:45:58 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ connect();
 //Creamos el enrutador
 const app = express();
 
-//All cors
+//All cors allow
 app.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
